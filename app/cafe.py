958ee96d891573359bcbca38487ed21dd2b461e2
= <=>
2025-05-11 16:24:16 +0300

    implement py-lockdown-cafe

--- app/cafe.py
+++ app/cafe.py
@@ -1,6 +1,6 @@
 import datetime
 from typing import Dict, Any
-from .errors import VaccineError, NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
+from .errors import NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
 
 
 class Cafe:
